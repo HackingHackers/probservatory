a4e5d2e6aa10343c3370f73ad7bfc4105b854bb4
HackingHackers <52874216+HackingHackers@users.noreply.github.com>
2024-10-06 18:20:25 -0400

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -12,28 +12,7 @@ export const headerData = {
     },
     {
       text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      href: getBlogPermalink(),
     },
     {
       text: 'Support',
